fbc98bdfbae4d03b259959a26a40036c3e93e202
Anshul <anshulbhaskar50@gmail.com>
2025-08-17 02:04:56 +0530

    Navbar-home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,8 +10,8 @@ const Navbar = () => {
     <div className='navbar'>
         <img src={logo} alt="Logo" className="logo" />
         <ul className="navbar-menu">
-            <li><Link to="home" smooth={true} offset={0} duration={500}>Home</Link></li>
-            <li><Link to="about" smooth={true} offset={-260} duration={500}>About Me</Link></li>
+            <li><Link to="home" smooth={true} offset={-100} duration={500}>Home</Link></li>
+            <li><Link to="about" smooth={true} offset={-58} duration={500}>About Me</Link></li>
             <li><Link to="skills" smooth={true} offset={-150} duration={500}>Skills</Link></li>
             <li><Link to="projects" smooth={true} offset={-260} duration={500}>Projects</Link></li>
             <li><Link to="contact" smooth={true} offset={-260} duration={500}>Contact</Link></li>
